de3fe635d2dbc50c985201bd2efe6d5cc48f66ce
trbaonop <161367764+trbaonop@users.noreply.github.com>
2024-08-27 10:08:51 +0700

    Add files via upload

--- cute/main.js
+++ cute/main.js
@@ -7,7 +7,27 @@ built_in
 5,set interval
 
 */
+function showmorebuttons() {
+    var extraButtonDiv = document.getElementById("thêm-nút");
 
+    // Tạo nút mới 1
+    var newbutton1 = document.createElement("button");
+    newbutton1.innerHTML = "Nút mới 1";
+    newbutton1.onclick = function() {
+        alert("Đây là nút mới 1");
+    };
+    extraButtonDiv.appendChild(newbutton1); // Thêm nút mới 1 vào div
+
+    // Tạo nút mới 2
+    var newbutton2 = document.createElement("button");
+    newbutton2.innerHTML = "Nút mới 2";
+    newbutton2.onclick = function() {
+        alert("Đây là nút mới 2");
+    };
+    extraButtonDiv.appendChild(newbutton2); // Thêm nút mới 2 vào div
+}
+    
+
+console.log("xin chao")
 var hai =true
-
 alert(hai)
